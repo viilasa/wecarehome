f3e46991cf5b7ef75119f84867e6de908a8b63a0
viilasa <viilasacontact@gmail.com>
2025-02-19 00:02:52 +0530

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Hero from './components/Hero';
 import WhyUs from './components/WhyUs';
 import HowWeWork from './components/HowWeWork';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
 import { ThemeProvider } from './context/ThemeContext';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <WhyUs />
         <HowWeWork />
         <Contact />
+        <Footer />
       </div>
     </ThemeProvider>
   );
